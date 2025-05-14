e0219735ea55780380e881f04e10a2748ee477e1
Alex <alexcantsed@hotmail.com>
2025-05-14 17:08:54 -0500

    fix: shared services and logo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGreen: '#1ACD81',
+      },
+    },
+  },
+  plugins: [],
+}
+
